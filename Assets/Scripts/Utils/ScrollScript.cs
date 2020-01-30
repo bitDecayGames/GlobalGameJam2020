ae950c6890cf9befac7ee28709e5a7fb242b1786
Tanner <Shadoninja@gmail.com>
2020-01-30 06:39:23 -0700

    credits added

---
+++ Assets/Scripts/Utils/ScrollScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScrollScript : MonoBehaviour
+{
+
+    public float ScrollSpeed;
+    public float EndTime;
+    public float timePassed;
+
+    private Vector3 pos;
+
+    // Use this for initialization
+    void Start()
+    {
+        pos = new Vector3();
+        timePassed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        pos.Set(transform.position.x, transform.position.y, transform.position.z);
+        pos.y += ScrollSpeed * Time.deltaTime;
+        transform.position = new Vector3(pos.x, pos.y, pos.z);
+
+        if (timePassed >= EndTime)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
