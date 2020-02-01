ce71100bfc799a21f58a1ca73972a8c3768c7923
Logan Moore <monday.hopscotch@gmail.com>
2020-02-01 11:23:18 -0700

    initial tile parsing

---
+++ Assets/MapLoader.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SuperTiled2Unity;
+using UnityEngine;
+
+public class MapLoader : MonoBehaviour
+{    
+    private SuperMap _map;
+    private SuperTileLayer _baseDataLayer;
+    public GameObject tilePrefab;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _map = FindObjectOfType<SuperMap>();
+        if (_map == null)
+        {
+            Debug.Log("Unable to find tilemap... What did you do...");
+            return;
+        }
+
+        _baseDataLayer = _map.transform.Find("Grid").Find("data").GetComponent<SuperTileLayer>();
+
+        // Rig up all the cardinal directions. THIS LOOP ASSUMES some things:
+        // children are in Columns from left to right
+        for (var i = 0; i < _baseDataLayer.transform.childCount; i++)
+        {
+            var currentTile = _baseDataLayer.transform.GetChild(i);
+            currentTile.gameObject.AddComponent<Tile>();
+            if (i % _map.m_Height != 0)
+            {
+                currentTile.GetComponent<Tile>().north = _baseDataLayer.transform.GetChild(i - 1).GetComponent<Tile>();
+                _baseDataLayer.transform.GetChild(i - 1).GetComponent<Tile>().south = currentTile.GetComponent<Tile>();
+            }
+
+            if ((i - _map.m_Height) >= 0)
+            {
+                currentTile.GetComponent<Tile>().west = _baseDataLayer.transform.GetChild(i - _map.m_Height).GetComponent<Tile>();
+                _baseDataLayer.transform.GetChild(i - _map.m_Height).GetComponent<Tile>().east = currentTile.GetComponent<Tile>();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/_Game/Art/Tilesets/roadsTiles.tsx
+++ Assets/_Game/Art/Tilesets/roadsTiles.tsx
@@ -1,4 +1,70 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.2" name="mapTiles" tilewidth="80" tileheight="80" tilecount="24" columns="8">
  <image source="roads.png" width="640" height="240"/>
+ <tile id="4">
+  <properties>
+   <property name="conns" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="conns" type="int" value="14"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="conns" type="int" value="12"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="conns" type="int" value="10"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="conns" type="int" value="7"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="conns" type="int" value="15"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="conns" type="int" value="13"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="conns" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="entrance" value="8"/>
+   <property name="name" value="house"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="conns" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="conns" type="int" value="11"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="conns" type="int" value="9"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="conns" type="int" value="0"/>
+  </properties>
+ </tile>
 </tileset>
