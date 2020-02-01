5698ee393f83303eeea1f6fd0646cf1daea23a4e
Logan Moore <monday.hopscotch@gmail.com>
2020-02-01 09:58:42 -0700

    getting things set up

---
+++ Assets/_Game/Art/Tilesets/roadsTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="mapTiles" tilewidth="80" tileheight="80" tilecount="24" columns="8">
+ <image source="roads.png" width="640" height="240"/>
+</tileset>
