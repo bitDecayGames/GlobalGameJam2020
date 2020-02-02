72bc847cc723dca051628bdd9a98c1333fa28b02
Logan Moore <monday.hopscotch@gmail.com>
2020-02-01 21:33:43 -0700

    rig up Erik's map. meta files

--- Assets/_Game/Art/Tilesets/roadsTiles.tsx
+++ Assets/_Game/Art/Tilesets/roadsTiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="mapTiles" tilewidth="80" tileheight="80" tilecount="72" columns="8">
+<tileset version="1.2" tiledversion="1.2.1" name="mapTiles" tilewidth="80" tileheight="80" tilecount="72" columns="8">
  <image source="roads.png" width="640" height="720"/>
  <tile id="0">
   <properties>
@@ -25,45 +25,93 @@
    <property name="type" value="ROAD"/>
   </properties>
  </tile>
+ <tile id="4">
+  <properties>
+   <property name="conns" value="6"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="conns" value="14"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="conns" value="12"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="conns" value="10"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
  <tile id="8">
   <properties>
-   <property name="conns" value=""/>
+   <property name="conns" value="7"/>
    <property name="type" value="ROAD"/>
   </properties>
  </tile>
  <tile id="9">
   <properties>
-   <property name="conns" value=""/>
+   <property name="conns" value="15"/>
    <property name="type" value="ROAD"/>
   </properties>
  </tile>
  <tile id="10">
   <properties>
-   <property name="conns" value=""/>
+   <property name="conns" value="13"/>
    <property name="type" value="ROAD"/>
   </properties>
  </tile>
  <tile id="11">
   <properties>
-   <property name="conns" value=""/>
+   <property name="conns" value="5"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="conns" value="7"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="conns" value="15"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="conns" value="13"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="conns" value="5"/>
    <property name="type" value="ROAD"/>
   </properties>
  </tile>
  <tile id="16">
   <properties>
-   <property name="conns" value=""/>
+   <property name="conns" value="3"/>
    <property name="type" value="ROAD"/>
   </properties>
  </tile>
  <tile id="17">
   <properties>
-   <property name="conns" value=""/>
+   <property name="conns" value="11"/>
    <property name="type" value="ROAD"/>
   </properties>
  </tile>
  <tile id="18">
   <properties>
-   <property name="conns" value=""/>
+   <property name="conns" value="9"/>
    <property name="type" value="ROAD"/>
   </properties>
  </tile>
@@ -73,6 +121,240 @@
    <property name="entrance" value="8"/>
    <property name="name" value="house"/>
    <property name="type" value="DOOR"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="conns" value="3"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="conns" value="11"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="conns" value="9"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="conns" value="0"/>
+   <property name="type" value="SOLID"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="conns" value="6"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="conns" value="14"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="conns" value="12"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="conns" value="6"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="conns" value="14"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="conns" value="12"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="conns" value="7"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="conns" value="0"/>
+   <property name="type" value="SOLID"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="conns" value="13"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="conns" value="7"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="conns" value="15"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="conns" value="15"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="conns" value="3"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="conns" value="11"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="conns" value="9"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="conns" value="3"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="conns" value="11"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="conns" value="9"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="conns" value="13"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="conns" value="15"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="conns" value="15"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="conns" value="15"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="conns" value="15"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="conns" value="15"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="conns" value="15"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="conns" value="15"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="conns" value="15"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="conns" value="15"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="conns" value="5"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="conns" value="10"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="conns" value="5"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="conns" value="10"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="conns" value="8"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="conns" value="13"/>
+   <property name="type" value="ROAD"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="conns" value="14"/>
+   <property name="type" value="ROAD"/>
   </properties>
  </tile>
 </tileset>
