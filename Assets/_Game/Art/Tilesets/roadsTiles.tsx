9e61a7acb987518531f903414ccdf946aa45bd24
nandemoguy <nandemoguy@gmail.com>
2020-02-01 19:50:21 -0700

    poc stage

--- Assets/_Game/Art/Tilesets/roadsTiles.tsx
+++ Assets/_Game/Art/Tilesets/roadsTiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="mapTiles" tilewidth="80" tileheight="80" tilecount="24" columns="8">
- <image source="roads.png" width="640" height="240"/>
+<tileset version="1.2" tiledversion="1.3.2" name="mapTiles" tilewidth="80" tileheight="80" tilecount="24" columns="4">
+ <image source="roads.png" width="320" height="400"/>
  <tile id="0">
   <properties>
    <property name="conns" value="6"/>
@@ -27,25 +27,25 @@
  </tile>
  <tile id="4">
   <properties>
-   <property name="conns" type="int" value="6"/>
+   <property name="conns" value=""/>
    <property name="type" value="ROAD"/>
   </properties>
  </tile>
  <tile id="5">
   <properties>
-   <property name="conns" type="int" value="14"/>
+   <property name="conns" value=""/>
    <property name="type" value="ROAD"/>
   </properties>
  </tile>
  <tile id="6">
   <properties>
-   <property name="conns" type="int" value="12"/>
+   <property name="conns" value=""/>
    <property name="type" value="ROAD"/>
   </properties>
  </tile>
  <tile id="7">
   <properties>
-   <property name="conns" type="int" value="10"/>
+   <property name="conns" value=""/>
    <property name="type" value="ROAD"/>
   </properties>
  </tile>
@@ -69,82 +69,10 @@
  </tile>
  <tile id="11">
   <properties>
-   <property name="conns" value=""/>
-   <property name="type" value="ROAD"/>
-  </properties>
- </tile>
- <tile id="12">
-  <properties>
-   <property name="conns" type="int" value="7"/>
-   <property name="type" value="ROAD"/>
-  </properties>
- </tile>
- <tile id="13">
-  <properties>
-   <property name="conns" type="int" value="15"/>
-   <property name="type" value="ROAD"/>
-  </properties>
- </tile>
- <tile id="14">
-  <properties>
-   <property name="conns" type="int" value="13"/>
-   <property name="type" value="ROAD"/>
-  </properties>
- </tile>
- <tile id="15">
-  <properties>
-   <property name="conns" type="int" value="5"/>
-   <property name="type" value="ROAD"/>
-  </properties>
- </tile>
- <tile id="16">
-  <properties>
-   <property name="conns" value=""/>
-   <property name="type" value="ROAD"/>
-  </properties>
- </tile>
- <tile id="17">
-  <properties>
-   <property name="conns" value=""/>
-   <property name="type" value="ROAD"/>
-  </properties>
- </tile>
- <tile id="18">
-  <properties>
-   <property name="conns" value=""/>
-   <property name="type" value="ROAD"/>
-  </properties>
- </tile>
- <tile id="19">
-  <properties>
    <property name="conns" value="0"/>
    <property name="entrance" value="8"/>
    <property name="name" value="house"/>
    <property name="type" value="DOOR"/>
-  </properties>
- </tile>
- <tile id="20">
-  <properties>
-   <property name="conns" type="int" value="3"/>
-   <property name="type" value="ROAD"/>
-  </properties>
- </tile>
- <tile id="21">
-  <properties>
-   <property name="conns" type="int" value="11"/>
-   <property name="type" value="ROAD"/>
-  </properties>
- </tile>
- <tile id="22">
-  <properties>
-   <property name="conns" type="int" value="9"/>
-   <property name="type" value="ROAD"/>
-  </properties>
- </tile>
- <tile id="23">
-  <properties>
-   <property name="conns" type="int" value="0"/>
-   <property name="type" value="SOLID"/>
   </properties>
  </tile>
 </tileset>
---
+++ Assets/_Game/Levels/roads.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="roads" tilewidth="80" tileheight="80" tilecount="20" columns="4">
+ <image source="../Art/Tilesets/roads.png" width="320" height="400"/>
+</tileset>
